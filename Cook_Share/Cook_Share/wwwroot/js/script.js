2d2981eb62d0f7537ecfe80aa1419663e9f09833
PavelPetrochenkov <pavlo.petrochenkov@nure.ua>
2019-11-12 10:20:42 +0300

    + btn

---
+++ Cook_Share/Cook_Share/wwwroot/js/script.js
@@ -0,0 +1,88 @@
+﻿
+
+function addContentMyDish() {
+    document.getElementById("left__content").innerHTML = `
+    <div class="content__mydishes">
+              <div class="add-content__mydish">
+                <img src="../img/plus.png" alt="" class="photo__add" />
+                <button class="button__add-dish">Добавить</button>
+              </div>
+
+              <div class="content__mydish">
+                <img src="../img/photo-food-1.png" alt="" class="photo__dish" />
+                <h1></h1>
+              </div>
+
+              <div class="content__mydish">
+                <img src="../img/photo-food-1.png" alt="" class="photo__dish" />
+                <h1></h1>
+              </div>
+
+              <div class="content__mydish">
+                <img src="../img/photo-food-1.png" alt="" class="photo__dish" />
+                <h1></h1>
+              </div>
+
+              <div class="content__mydish">
+                <img src="../img/photo-food-1.png" alt="" class="photo__dish" />
+                <h1></h1>
+              </div>
+
+              <div class="content__mydish">
+                <img src="../img/photo-food-1.png" alt="" class="photo__dish" />
+                <h1></h1>
+              </div>
+            </div>`;
+}
+
+function addContentMyFavorite() {
+    document.getElementById("left__content").innerHTML = `
+    <div class="content__mydishes">
+              
+
+              <div class="content__mydish">
+                <img src="../img/photo-food-1.png" alt="" class="photo__dish" />
+                <h1></h1>
+              </div>
+
+              <div class="content__mydish">
+                <img src="../img/photo-food-1.png" alt="" class="photo__dish" />
+                <h1></h1>
+              </div>
+
+              <div class="content__mydish">
+                <img src="../img/photo-food-1.png" alt="" class="photo__dish" />
+                <h1></h1>
+              </div>
+
+              <div class="content__mydish">
+                <img src="../img/photo-food-1.png" alt="" class="photo__dish" />
+                <h1></h1>
+              </div>
+
+              <div class="content__mydish">
+                <img src="../img/photo-food-1.png" alt="" class="photo__dish" />
+                <h1></h1>
+              </div>
+            </div>`;
+}
+
+
+/* ---------- btn add photo ------------------*/
+
+let addPhotoArea = document.getElementById('addPhotoArea');
+let addPhotoBtn = document.getElementById('addPhotoBtn');
+
+
+addPhotoArea.onmouseover = function () {
+    addPhotoBtn.hidden = false;
+}
+
+addPhotoArea.onmouseout = function () {
+    addPhotoBtn.hidden = true;
+}
+
+addPhotoBtn.onmouseover = function () {
+    addPhotoBtn.hidden = false;
+}
+
